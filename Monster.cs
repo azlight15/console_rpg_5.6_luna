e9d9271862f6f342b789539091b2704c2bb9207a
AzLight <104720079+azlight15@users.noreply.github.com>
2026-09-15 16:20:55 +0800

    feat: add monster evasion data for combat v0.3

--- Monster.cs
+++ Monster.cs
@@ -3,8 +3,8 @@ namespace Console_RPG;
 /// <summary>
 /// 战斗中敌人的数据模型。
 ///
-/// v0.3 开始为怪物加入类型信息，方便未来扩展特殊能力、技能和不同战斗行为。
-/// 当前阶段仍主要保存基础战斗数据。
+/// Luna v0.3.0 开始加入怪物战斗定位数据。
+/// 怪物不再只是名称和数值，而会逐渐拥有自己的战斗特点。
 /// </summary>
 public class MonsterStatistics
 {
@@ -15,4 +15,10 @@ public class MonsterStatistics
     public double MaxHp { get; set; }
     public double Attack { get; set; }
     public double ExpReward { get; set; }
+
+    /// <summary>
+    /// 怪物闪避概率。
+    /// 用于表现不同怪物的战斗风格，例如野狼更灵活。
+    /// </summary>
+    public double EvasionRate { get; set; }
 }
